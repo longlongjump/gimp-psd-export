5114848aa1aa7cb35cbc91448cbe6dda0a9613df
Nero <nerohellier@gmail.com>
2011-01-17 13:57:56 +0200

    macosx

---
+++ plugin_install_macos.sh
@@ -0,0 +1,4 @@
+cp export.py  ~/Library/Application\ Support/Gimp/plug-ins/
+chmod +x  ~/Library/Application\ Support/Gimp/plug-ins/export.py
+killall -9 Gimp
+/Applications/Gimp.app/Contents/MacOS/Gimp&
